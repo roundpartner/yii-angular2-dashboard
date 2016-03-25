ecb1268634d19549755ba4835f31908255899033
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2016-03-25 16:00:46 +0000

    Register Angular distribution

--- src/components/Dashboard.php
+++ src/components/Dashboard.php
@@ -4,5 +4,16 @@ class Dashboard extends CApplicationComponent
 {
     public function register()
     {
+        $cs = Yii::app()->getClientScript();
+        // IE Polyfills
+        $cs->registerScriptFile('/dist/lib/es6-shim.min.js', Yii::app()->clientScript->coreScriptPosition);
+        $cs->registerScriptFile('/dist/lib/system-polyfills.js', Yii::app()->clientScript->coreScriptPosition);
+        $cs->registerScriptFile('/dist/lib/shims_for_IE.js', Yii::app()->clientScript->coreScriptPosition);
+
+        $cs->registerScriptFile('/dist/lib/angular2-polyfills.js', Yii::app()->clientScript->coreScriptPosition);
+        $cs->registerScriptFile('/dist/lib/system.src.js', Yii::app()->clientScript->coreScriptPosition);
+        $cs->registerScriptFile('/dist/lib/Rx.js', Yii::app()->clientScript->coreScriptPosition);
+        $cs->registerScriptFile('/dist/lib/angular2.dev.js', Yii::app()->clientScript->coreScriptPosition);
+        $cs->registerScriptFile('/dist/lib/http.dev.js', Yii::app()->clientScript->coreScriptPosition);
     }
 }
